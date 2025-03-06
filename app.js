c1eb05125345e826a5f02b18acb99f502df4e2f5
BADHON512 <mylaptop512@gmail.com>
2025-03-06 11:00:33 +0600

    console.log removed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
 });
 
 const users = {}; // for user store
-console.log(users, "usersvai");
+
 
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
