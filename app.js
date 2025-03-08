6fdd9b13b86c240b0092b82bbbd93013c6bba155
BADHON512 <mylaptop512@gmail.com>
2025-03-08 22:15:29 +0600

    ss

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:"*",
+    origin:"http://localhost:3000/",
     methods: ["GET", "POST"],
     credentials: true,
   },
